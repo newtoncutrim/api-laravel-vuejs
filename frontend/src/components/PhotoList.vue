a5cd4dc5d1c70a0f4c7b15e7dc64b0fbd55e99a7
newtoncutrim <newtoncutrim777@gmail.com>
2023-11-28 16:06:03 -0300

    Fix: na exibição fotos

--- frontend/src/components/PhotoList.vue
+++ frontend/src/components/PhotoList.vue
@@ -53,7 +53,7 @@
                   <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1><span>{{ this.editingPhoto.id }}</span>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                           ref="myModal"></button>
                       </div>
